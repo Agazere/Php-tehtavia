28e29fe3b2a2840bf50698c1e44ec7efdb86d5b0
Katja Hyrynkangas <katja.hyrynkangas@hotmail.com>
2018-04-12 19:41:18 +0300

    Form
    Numeronarvaustaulukon koodi

---
+++ Katja_tahtitehtava.php
@@ -0,0 +1,6 @@
+<form action="Katja_tahtitehtava.php" method="post">
+<h4>Tähtitehtävä</h4>
+ <p>Montako tähteä laitetaan?</p>
+ <input type="number" name="tahti" />
+ <p><input type="submit" /></p>
+</form>
